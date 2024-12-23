584716e8c27e34f874adcdc974280ff312cbe9ec
Shaurya Thareja <shauryathareja0323@gmail.com>
2024-12-23 15:21:12 +0530

    vectors

--- Vectors/vectorFunctions.cpp
+++ Vectors/vectorFunctions.cpp
@@ -30,6 +30,14 @@ int main(){
     cout<<vec.back()<<endl; //Prints the last value of the vector
 
     cout<<vec.at(3)<<endl; //Prints the value at the given index
-    
 
+    vector <int> vec2;
+    vec2.push_back(1);
+    vec2.push_back(2);
+    vec2.push_back(3);
+
+    cout<<"Size of the vector 3 is:"<<vec2.size()<<endl;
+    cout<<"Capacity of the vector 3 is:"<<vec2.capacity()<<endl; //This is because everytime a vector is internaly 
+                                                                 //lacking space it's capacity is doubled rather than 
+                                                                 //being increased by one.  
 }
