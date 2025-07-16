159ad78b8ef3d84a32196c743560024b71648146
Shaurya Thareja <shauryathareja0323@gmail.com>
2025-07-16 22:00:30 +0530

    Deque

---
+++ STL_Library/Deque/Deque_init.cpp
@@ -0,0 +1,15 @@
+//Deque and list can be implemeted simillarily but difference between them is that list is
+//implemented as a doubly linked list internally while deque is implemented with the help of 
+//dynamic arrays.
+#include<iostream>
+#include<deque>
+using namespace std;
+
+int main(){
+    deque<int> d = {1,2,3};
+
+    for(int i:d){
+        cout<<i<<" ";
+    }
+    return 0;
+}
