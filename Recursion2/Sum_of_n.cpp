7c4bf5ce512ec6ce84a523775823706613086ccd
Shaurya Thareja <shauryathareja0323@gmail.com>
2025-09-20 16:07:17 +0530

    k

--- Recursion2/Sum_of_n.cpp
+++ Recursion2/Sum_of_n.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int sum_of_n(int n){
-    if(n==0){
-        return 0;
+    if(n==1){
+        return 1;
     }
     int sum = n+sum_of_n(n-1);
     return sum;
 }
 
 int main(){
-    int n = 0;
+    int n = 3;
     int r = sum_of_n(n);
     cout<<r<<endl;
     return 0;
