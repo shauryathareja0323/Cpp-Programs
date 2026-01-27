bfa55ce7065a69e09e6c5a361a4f6ed84b83cc44
Shaurya Thareja <shauryathareja0323@gmail.com>
2026-01-27 13:02:06 +0530

    k

--- OOPS/constructor.cpp
+++ OOPS/constructor.cpp
@@ -9,12 +9,22 @@ private:
     double salary;
 
 public:
+    // We can have multiple constructors of same name until they have different parameters.
+    // Non Parameterized Constructor
     Teacher(){ //This is an example of constructor which will always run without compiler creating it's own constructor, this time we have created our own constructor.
         cout<<"Hi I am a constructor"<<endl;
         dept = "Computer Science";
     }
 
-    string name = "Shaurya";
+    // Parameterized Constructor
+    Teacher(string n, string d, string s, double sal){ //This is an example of constructor which will always run without compiler creating it's own constructor, this time we have created our own constructor.
+        name = n;
+        dept = d;
+        subject = s;
+        salary = sal;
+    }
+
+    string name;
     string dept;
     string subject;
 
@@ -30,26 +40,31 @@ public:
     double getSalary(){
         return salary;
     }
+
+    void get_info(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Subject: "<<subject<<endl;
+    }
 };
 
 int main(){
-    Teacher t1; //constructor call, whenever a new object is created.
-    t1.name = "Aman";
-    t1.subject = "C++";
+    Teacher t1("Shaurya", "CS", "C++", 250000); //constructor call, whenever a new object is created.
+    t1.get_info();
     //t1.dept = "cs";
 
-    cout<<t1.name<<endl;
-    cout<<t1.subject<<endl;
-    cout<<t1.dept<<endl;
 
-    t1.changeDept("elec");
-    t1.setSalary(3000);
+    // cout<<t1.name<<endl;
+    // cout<<t1.subject<<endl;
+    // cout<<t1.dept<<endl;
 
-    cout<<t1.dept<<endl;
+    // t1.changeDept("elec");
+    // t1.setSalary(3000);
 
-    double salary = t1.getSalary();
+    // cout<<t1.dept<<endl;
 
-    cout<<salary<<endl;
+    // double salary = t1.getSalary();
+
+    // cout<<salary<<endl;
     
 
     return 0;
