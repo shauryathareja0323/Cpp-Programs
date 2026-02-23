40af9fc858585b02ff508fcc4b689ba99b2ad2be
Shaurya Thareja <shauryathareja0323@gmail.com>
2026-02-23 19:25:52 +0530

    k

--- OOPS/shallow_deep_copy.cpp
+++ OOPS/shallow_deep_copy.cpp
@@ -5,27 +5,31 @@ using namespace std;
 class Student{
 public:
     string name;
-    double cgpa;
+    double* cgpaPtr;
+
 
     Student(string name, double cgpa) {
         this->name = name;
-        this->cgpa = cgpa;
+        cgpaPtr = new double;
+        *cgpaPtr = cgpa;
     }
 
     Student(Student &obj){
         this->name=obj.name;
-        this->cgpa=obj.cgpa;
+        this->cgpaPtr=obj.cgpaPtr;
     }
 
     void getInfo(){
         cout<<"Name: "<<name<<endl;
-        cout<<"CGPA: "<<cgpa<<endl;
+        cout<<"CGPA: "<< *cgpaPtr<<endl;
     }
 };
 
 int main(){
     Student s1("Rahul Kumar", 6.5);
     Student s2(s1);
+    s1.getInfo();
+    *(s2.cgpaPtr) = 9.2;
     s1.getInfo();
     s2.getInfo();
     return 0;
