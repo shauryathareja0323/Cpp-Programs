528b6250a0c62d21e5214eab08ae3735e7e67f41
Shaurya Thareja <shauryathareja0323@gmail.com>
2026-02-26 10:21:09 +0530

    k

---
+++ LinkedList/linkedList.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class List{
+    Node* head;
+    Node* tail;
+
+public:
+    List(){
+        head = NULL;
+        tail = NULL;
+    }
+
+    void push_front(int val){
+        Node* newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode;
+            return;
+        }
+        else{
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void push_back(int val){
+        Node* newNode = new Node(val);
+
+        if(head==NULL){
+            head=tail=newNode;
+        }
+        else{
+            tail->next=newNode;
+            tail=newNode;
+        }
+    }
+
+    void printLL(){
+        Node* temp = head;
+        while(temp != NULL){
+            cout<<temp->data<<"->";
+            temp = temp->next;
+        }
+        cout<<"NULL"<<endl;
+    }
+};
+
+
+int main(){
+    List ll;
+
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+    ll.push_back(4);
+
+    ll.printLL();
+
+    return 0;
+}
